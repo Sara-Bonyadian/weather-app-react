cc44c22c94c2a00dd3807e3c63a01ee2b81c8fdd
Sara Bonyadian <sara.bonyadian@gmail.com>
2024-01-28 22:01:06 -0500

    add HTML tag

--- src/Weather.js
+++ src/Weather.js
@@ -3,11 +3,43 @@ import axios from "axios";
 import "./Weather.css";
 
 export default function WeatherSearch() {
-    return(<div className="weather">
-        <h1>New York</h1>
-        <ul>
-            <li>Wednesday 7:00</li>
-            <li>Mostly cloudy</li>
-        </ul>
-    </div>);
+  return (
+    <div className="weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 7:00</li>
+        <li>Mostly cloudy</li>
+      </ul>
+      <div class="row">
+        <div class="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+            alt="mostly cloudy"
+          />{" "}
+          6 C
+        </div>
+        <div class="col-6">
+          <ul>
+            <li>Precipitation: 0%</li>
+            <li>Humidity: 91%</li>
+            <li>Wind: 24 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
