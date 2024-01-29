2f740e862e5ce70e19b6846efa8e5ab1d5e3ec6d
Sara Bonyadian <sara.bonyadian@gmail.com>
2024-01-28 20:41:24 -0500

    remove unessary files

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import "./index.css";
 import React from "react";
 import WeatherSearch from "./WeatherSearch";
+import "./App.css"
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <WeatherSearch />
+      <div className="container">
+        <h1>Weather App</h1>
+        <WeatherSearch />
+        <footer>
+          this project was coded by{" "}
+          <a href="https://www.linkedin.com/in/sarabonyadian/"> Sara Boni</a>{" "}
+          and is{" "}
+          <a href="https://github.com/Sara-Bonyadian/weather-app-react.git">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
