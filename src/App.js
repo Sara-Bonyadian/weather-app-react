0606dfc5ab7ff4ac8f457e52e373ffa41c1dcf7d
Sara Bonyadian <sara.bonyadian@gmail.com>
2024-01-28 20:58:04 -0500

    add footer

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import WeatherSearch from "./WeatherSearch";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Weather App</h1>
-      <WeatherSearch />
+    <div className="container">
+      <div className="App">
+        <h1>Weather App</h1>
+        <WeatherSearch />
+      </div>
+      <footer>
+        This project was coded by <a href="">Sara Boni</a> and is{" "}
+        <a href="">open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
